a4d1a82be9bd59ebfcc74e4a3c0b1bbc0a647bdf
stamich <o4tgxzhz>
2019-09-18 17:26:55 +0200

    init Commit

---
+++ ChartEngine/cmake-build-debug/CMakeFiles/ChartCore.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChartCore.dir/main.cpp.obj"
+  "ChartCore.pdb"
+  "ChartCore.exe"
+  "ChartCore.exe.manifest"
+  "ChartCore.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChartCore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
