7150c38b801ff6022b502f18e43a94fe47297541
stamich <o4tgxzhz>
2019-09-18 19:00:33 +0200

    Class templates and configuration added

--- ChartEngine/main.cpp
+++ ChartEngine/main.cpp
@@ -2,9 +2,32 @@
 // Created by Michal S. on 18.07.2019.
 //
 
+#include <src/ArgumentParser.h>
+
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[])
+{
+    try {
+        base::ArgumentParser argParser{argc, argv};
+        framework::TestRunner testRunner{std::move(argParser)};
+
+        testRunner.run();
+
+    } catch (const std::system_error& systemError) {
+        std::cerr << "Caught std::system_error!" << std::endl;
+        std::cerr << "  Code:    " << systemError.code() << std::endl;
+        std::cerr << "  Message: " << systemError.what() << std::endl;
+
+    } catch (const std::exception& exception) {
+        std::cerr << "Caught std::exception!" << std::endl;
+        std::cerr << "  Message: " << exception.what() << std::endl;
+        return -1;
+
+    } catch (...) {
+        std::cerr << "Caught unknown exception!" << std::endl;
+        return -1;
+    }
+
     return 0;
 }
